0972ca0d74ad2e403f68961b0d9be620c78166c5
liby85 <75899657@qq.com>
2025-07-09 12:57:47 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -46,7 +46,9 @@ UPDATE_PACKAGE() {
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 #UPDATE_PACKAGE "nps" "feifan1113/luci-app-nps" "main"
-UPDATE_PACKAGE "luci-app-npc" "yufan6663/luci-app-npc" "main"
+UPDATE_PACKAGE "luci-app-npc" "yufan666/luci-app-npc" "main"
+UPDATE_PACKAGE "luci-app-ddnsto" "goIntoAction/luci-app-ddnsto" "main"
+UPDATE_PACKAGE "luci-app-adguardhome" "rufengsuixing/luci-app-adguardhome" "main"
 
 UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 UPDATE_PACKAGE "theme-kucat" "sirpdboy/luci-theme-kucat" "js"
